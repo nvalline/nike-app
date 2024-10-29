c628d9036d96aa6f6be424d5b50b49b80893598e
vall4251n <58278138+nvalline@users.noreply.github.com>
2024-10-29 12:57:43 -0600

    Add desktop nav hover effect

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -16,7 +16,7 @@ const Nav = () => {
 						<li key={link.label}>
 							<a
 								href={link.href}
-								className='font-montserrat leading-normal text-lg text-slate-gray'
+								className='font-montserrat leading-normal text-lg text-slate-gray hover:text-coral-red'
 							>
 								{link.label}
 							</a>
